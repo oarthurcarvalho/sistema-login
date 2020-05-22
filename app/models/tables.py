0f39b6825a45255dbe30d9f8112afc0af08f72b0
Arthur Carvalho <arthur.reis@live.com>
2020-05-22 02:26:53 -0300

    Integraçao SQLAlchemy e Bottle

--- app/models/tables.py
+++ app/models/tables.py
@@ -1,22 +1,18 @@
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-Base = declarative_base()
+from app import Base
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     username = Column(String(30), unique=True, nullable=False)
     password = Column(String(30), nullable=False)
     
-    def __repr__(self):
-        return username
+    def __init__(self, username,password):
+        self.username = username
+        self.password = password
     
-engine = create_engine('sqlite:///database.db')
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine) 
+    def __repr__(self):
+        return f'User: {self.username}'
 
 def insert_user(username, password):
     new_user = User(username=username, password=password)
